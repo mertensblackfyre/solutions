1a2bc8acf7c66c80d3ca107cb842236cf25bd314
amr8644 <amr8644@gmail.com>
2023-03-03 21:02:53 +0300

    first commit

--- app.js
+++
@@ -1,8 +0,0 @@
-/*jshint esversion: 6 */
-
-const nav = document.querySelector(".nav-toggle");
-const list = document.querySelector(".list");
-
-nav.addEventListener("click", () => {
-  list.classList.toggle("show-list");
-});
