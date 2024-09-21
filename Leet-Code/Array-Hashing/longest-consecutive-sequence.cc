2a0dab035b06f5c322309a60107d5c177c2ef314
nyx6965 <72759681+nyx6965@users.noreply.github.com>
2024-09-21 16:48:21 +0300

    longest cons

--- Leet-Code/Array-Hashing/longest-consecutive-sequence.cc
+++ Leet-Code/Array-Hashing/longest-consecutive-sequence.cc
@@ -6,19 +6,37 @@ class Solution {
 public:
   int longestConsecutive(std::vector<int> &nums) {
 
-    int ans = 0;
+    int ans = 1;
+    std::vector<int> arr = {};
     std::sort(nums.begin(), nums.end());
+    int front, back = 0;
+    front = 1;
 
-    for (int k = 0; k < nums.size(); ++k) {
-      if (nums[k + 1] == nums[k] + 1 || nums[k + 1] == nums[k]) {
-        ans++;
+    if (nums.size() == 0)
+      return 0;
+
+    while (front <= nums.size()) {
+      if (front <= nums.size()) {
+        if (nums[back] + 1 == nums[front]) {
+          ans++;
+        } else {
+          arr.push_back(ans);
+          ans = 1;
+        }
+        front++;
+        back++;
       } else {
         break;
       }
+    }
+
+    std::sort(arr.begin(), arr.end());
+    for (int k = 0; k < arr.size(); ++k) {
+    //  std::cout << arr[k] << "->";
     };
 
-    std::cout << "ANS->" << ans << std::endl;
-    return ans;
+    std::cout << std::endl << "Anst-->" << arr[arr.size() - 1];
+    return arr[arr.size() - 1];
   };
 };
 
@@ -28,6 +46,7 @@ int main() {
   std::vector<int> case1 = {100, 4, 200, 1, 3, 2};
   std::vector<int> case2 = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
   std::vector<int> case3 = {9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6};
-  a.longestConsecutive(case2);
   a.longestConsecutive(case1);
+  a.longestConsecutive(case2);
+  a.longestConsecutive(case3);
 }
