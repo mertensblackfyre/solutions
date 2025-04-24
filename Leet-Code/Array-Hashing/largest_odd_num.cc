6756a35c26e26281794b7ced08e3165f64a569f4
mertensblackfyre <72759681+mertensblackfyre@users.noreply.github.com.>
2025-04-24 21:41:53 +0300

    New

---
+++ Leet-Code/Array-Hashing/largest_odd_num.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+  int string_to_int(std::string str) {
+    int num = 0;
+    try {
+      num = std::stoi(str);
+    } catch (...) {
+      return -1;
+    }
+    return num;
+  };
+
+  std::string solver(std::string num) {
+
+    std::string n;
+    int max = 0;
+    for (char c : num) {
+      int nn = string_to_int(n);
+      if (nn % 2 != 0) {
+
+        if (nn > max) {
+          max = nn;
+        };
+      }
+      n += c;
+    };
+
+    if (max == 0) {
+      return "";
+    };
+
+    std::string s = std::to_string(max);
+
+    return s;
+  };
+  std::string largestOddNumber(std::string num) {
+
+    int n = string_to_int(num);
+    if (n % 2 != 0) {
+
+      return num;
+    } else {
+      return solver(num);
+    }
+
+    return "";
+  };
+};
+int main() {
+
+  std::string n = "2928327648";
+
+  Solution x;
+
+  std::cout << x.largestOddNumber(n) << std::endl;
+}
