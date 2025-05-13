357955a88b46242a3be53151487a687921690acd
mertensblackfyre <72759681+mertensblackfyre@users.noreply.github.com.>
2025-05-13 20:08:28 +0300

    More backtracking

--- Leet-Code/Backtracking/combination_sum2.cc
+++ Leet-Code/Backtracking/combination_sum2.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -19,20 +20,23 @@ public:
       res.push_back(perm);
       return;
     };
+
     if (sum > target) {
-      start--;
+      start++;
       return;
     }
+
     for (int i = start; i < nums.size(); i++) {
       perm.push_back(nums[i]);
-      backtrack(i, nums, target, perm);
+      backtrack(i+1, nums, target, perm);
       perm.pop_back();
     }
   };
-  std::vector<std::vector<int>> combinationSum(std::vector<int> &candidates,
-                                               int target) {
+  std::vector<std::vector<int>> combinationSum2(std::vector<int> &candidates,
+                                                int target) {
     res.clear();
     std::vector<int> perm;
+    std::sort(candidates.begin(),candidates.end());
     backtrack(0, candidates, target, perm);
     return res;
   };
@@ -42,14 +46,13 @@ int main() {
   Solution x;
 
   int target = 8;
-  std::vector<int> nums = {2, 3, 5};
+  std::vector<int> nums = {10, 1, 2, 7, 6, 1, 5};
 
-  std::vector<std::vector<int>> res = x.combinationSum(nums, target);
+  std::vector<std::vector<int>> res = x.combinationSum2(nums, target);
 
   for (auto x : res) {
-    for (int y : x) {
+    for (int y : x)
       std::cout << y;
-    }
     std::cout << std::endl;
   }
   return 0;
